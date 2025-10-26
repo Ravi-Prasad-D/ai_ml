a232c56c6f3b8ba306f82e838f39f36431343ecc
Ravi-Prasad-D <34828795+Ravi-Prasad-D@users.noreply.github.com>
2025-10-26 11:35:56 +0530

    Add webapp.py

--- webapp.py
+++ webapp.py
@@ -3,3 +3,22 @@ import pandas as pd
 import numpy as np
 ##title of the applicaiton
 st.title("hi rupika, how are you")
+
+#create a simple dataframe
+
+df = pd.DataFrame({
+    'first column':[1,2,3,4],
+    'second column':[10,20,30,40]
+})
+
+#Display the data frame
+st.write("Here is the dataframe")
+st.write(df)
+
+#create a line chart
+
+chart_data=pd.DataFrame(
+    np.random.randn(20,3),columns=['a','b','c']
+)
+st.line_chart(chart_data)
+
